6c6124bb9da14f7e8dc64f2c14b21f87735c6bc3
Davittt <ddonari@gmail.com>
2017-07-24 15:36:41 -0300

    First commit

---
+++ BusinessEntities/UserActivity.cs
@@ -0,0 +1,16 @@
+
+namespace BusinessEntities
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class UserActivity
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public string Username { get; set; }
+
+		public string Activity { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
